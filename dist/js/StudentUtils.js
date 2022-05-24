d7c0a2a43059f79bb25047271cc82dd4cad8d3e8
Gvozdoedko <uraradhenko007@gmail.com>
2022-05-24 17:59:38 +0300

    List V1.1.1

--- dist/js/StudentUtils.js
+++ dist/js/StudentUtils.js
@@ -3,46 +3,7 @@ let students = doListOfStudent(question);
 let questionMark;
 console.log(students);
 
-function main() {
-    do {
-        let question = +prompt(`Выберите операцию: 
-        1.получить самого преуспевающего студента
-        2.отсортировать студентов по успеваемости
-        3.посчитать среднюю оценку для каждого студента
-        4.получить студентов на отчисление
-        5.добавить новенького в группу
-        6.ввести оценку самостоятельно
-        7.Добавить случайного студента`);
-        switch (question) {
-            case 1:
-                console.log(getBestStudent(students));
-                break;
-            case 2:
-                console.log(bubbleSort(students));
-                break;
-            case 3:
-                console.log(addAvgMark(students));
-                break;
-            case 4:
-                console.log(listOfDeb(students));
-                break;
-            case 5:
-                console.log(newStudent(students));
-                break;
-            case 6:
-                questionMark = +prompt(`Какому студенту ввести оценку?`);
-                console.log(urMark(students));
-                break;
-            case 7:
-                console.log(addRandomStudent(students));
-                break;
 
-            default:
-                break;
-        }
-
-    } while (confirm('Do you want to continue?'));
-}
 
 
 
